1a562b0b0dff3c7e01a459be57557db839367fb4
QA <qa@QAs-iMac.local>
2018-09-17 18:19:48 +0300

    C conversion characters added

---
+++ The_NewBoston_Tutorials/Conversion_Characters.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+
+	printf("%s is the the best  %s ever\n", "Pavel", "programmer");
+	// %s is placeholder for strings
+
+	printf("I ate %d pizzas last night", 4);
+	// %d is placeholder for numbers ,whenever we want to use a float %f is used.
+	//if we want to use e.g. only two places after the decimal point '%.2f' is used.
+
+	return 0;
+
+
+
+}
